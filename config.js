54027ecbda8343448431a13684d44a362af962ce
jcognneii <92839654+jcognneii@users.noreply.github.com>
2021-10-20 11:01:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hế Lô!!!!',
+    introDesc: `Ỏ ai đây nhỉ !!!
+    Ai mà xinh thế nhỉ!!`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Đã được nhân quà 20/10 chưa ấy nhỉ 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Gòi :3',
+    btnNo: 'Đến bây giờ vẫn chưa :(',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
